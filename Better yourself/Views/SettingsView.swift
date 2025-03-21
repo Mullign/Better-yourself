3e77b60bf6d74e1aec55b9e6f5f563a74a19ab4a
Mull1gan <russellproductiongroup@gmail.com>
2025-03-21 07:44:19 -0400

    Initial Commit

--- Better yourself/Views/SettingsView.swift
+++ Better yourself/Views/SettingsView.swift
@@ -8,9 +8,93 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack (alignment: .leading, spacing: 20) {
+            
+            Text("Settings")
+                .font(.largeTitle)
+                .bold()
+            
+            List {
+                
+                // Rate the app
+                let reviewUrl = URL(string: "https://apps.apple.com/app/id6670766958?action=write-review")!
+                
+                
+                Link(destination: reviewUrl, label: {
+                    
+                    HStack {
+                        Image(systemName: "star.bubble")
+                        Text("Rate the app")
+                    }
+                    
+                })
+                
+                // Recommend the app
+                let shareUrl = URL(string: "https://apps.apple.com/app/id6670766958")!
+                
+                ShareLink(item: shareUrl) {
+                    HStack {
+                        Image(systemName: "arrowshape.turn.up.right")
+                        Text("Recommend the app")
+                    }
+                    
+                }
+                
+                // Contact
+                Button  {
+                    // Compose email
+                    let mailUrl = createMailUrl()
+                    
+                    if let mailUrl = mailUrl, UIApplication.shared.canOpenURL(mailUrl) {
+                        UIApplication.shared.open(mailUrl)
+                    }
+                    else {
+                        print("Couldn't open mail client")
+                    }
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "quote.bubble")
+                        Text("Submit feedback")
+                    }
+                    
+                }
+                
+                
+                // Privacy Policy
+                let privacyUrl = URL(string: "https://sandy-falcon-7b5.notion.site/Privacy-Policy-1bc21a541cd9801fa97df5b9a3db0916?pvs=4")!
+                
+                Link(destination: privacyUrl, label: {
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Privacy Policy")
+                    }
+                })
+                
+                
+            }
+            .listRowSeparator(.hidden)
+            .listStyle(.plain)
+            .tint(.black)
+        }
     }
+    
+    func createMailUrl() -> URL? {
+        
+        var mailUrlComponents = URLComponents()
+        mailUrlComponents.scheme = "mailto"
+        mailUrlComponents.path = "johnrussell@mull1gan.com"
+        mailUrlComponents.queryItems = [
+            URLQueryItem(name: "subject", value: "Feedback for A Better Day")
+        ]
+        
+        return mailUrlComponents.url
+    }
+    
 }
 
 #Preview {
